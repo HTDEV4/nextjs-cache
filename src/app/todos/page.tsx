120ff643716298c26db1d319f112e4826f99b566
Nguyen Vinh Hung <nguyenhung041104@gmail.com>
2025-05-25 17:24:10 +0700

    Bài 76 'use cache' directive trong phạm vi component - file

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,48 +1,38 @@
-import React from "react";
+"use cache"; // thằng này sẽ cache dữ liệu trong toàn file
 import Delete from "./_components/Delete";
 import Form from "./_components/Form";
 import View from "./_components/View";
 import { unstable_cacheLife as cacheLife } from "next/cache";
-import { cacheTag } from "next/dist/server/use-cache/cache-tag";
-// import { unstable_cache } from "next/cache";
-// import { cache } from "react";
 
 interface ITodo {
     id: string;
     title: string;
 }
 
-// const getTodoList = unstable_cache(async () => {
-//     const response = await fetch(`http://localhost:3001/todos`);
-//     const data = await response.json();
-//     return data;
-// }, [""], {
-//     tags: ["todo-list"], // On-demand revalidation, Cache invalidation
-//     // revalidate: 10, // Revalidate every 10 seconds, Time space
-// })
-
-// Cache này xử lí trong quá trình render giống như useMemo.
-// const getTodoList = cache(async () => {
-//     const response = await fetch(`http://localhost:3001/todos`);
-//     const data = await response.json();
-//     return data;
-// })
+interface IPost {
+    id: string;
+    title: string;
+}
 
 const getTodoList = async () => {
-    "use cache";
-    // cacheLife("seconds");
-    cacheTag("todo-list"); // On-demand revalidation, Cache invalidation
     const response = await fetch(`http://localhost:3001/todos`);
     const data = await response.json();
     return data;
 };
 
+const getPosts = async () => {
+    const response = await fetch(`http://localhost:3001/posts`);
+    return response.json();
+};
+
 export default async function TodoPage() {
+    cacheLife("minutes");
     const todoList = await getTodoList();
+    const postList = await getPosts();
     return (
         <div className="w-3/4 mx-auto py-3">
-            <h1 className="text-4xl">TodoList</h1>
-            <ul className="list-disc list-inside mt-3">
+            <h2 className="text-4xl">Todo List</h2>
+            <ul className="list-disc list-inside mt-2">
                 {
                     todoList.map((todo: ITodo) => (
                         <li key={todo.id} className="flex justify-between items-center">
@@ -50,6 +40,20 @@ export default async function TodoPage() {
                             <div>
                                 <View id={todo.id} />
                                 <Delete id={todo.id} />
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
+            <h2 className="text-4xl mt-3">Post List</h2>
+            <ul className="list-disc list-inside mt-2">
+                {
+                    postList.map((post: IPost) => (
+                        <li key={post.id} className="flex justify-between items-center">
+                            - {post.title}
+                            <div>
+                                <View id={post.id} />
+                                <Delete id={post.id} />
                             </div>
                         </li>
                     ))
